42a2b488a5a3733cc2b4d957c6f37b3a21f0af41
yuweiWu <986645906@qq.com>
2018-11-23 19:38:11 +0800

    create run.sh&predict.sh

---
+++ predict.sh
@@ -0,0 +1,2 @@
+export SQUAD_DIR=~/data/squad
+python ./evaluate-v1.1.py $SQUAD_DIR/dev-v1.1.json ./bert_large/predictions.json
---
+++ run.sh
@@ -0,0 +1,21 @@
+export SQUAD_DIR=~/data/squad
+export OUTPUT_DIR=./bert_large
+export BERT_MODEL_DIR=./bert-base-uncased
+export PYTHONPATH=./
+source activate pytorch
+CUDA_VISIBLE_DEVICES=1,2,3,6 python examples/run_squad.py \
+  --bert_model bert-large-uncased \
+  --do_train \
+  --do_predict \
+  --train_file $SQUAD_DIR/train-v1.1.json \
+  --predict_file $SQUAD_DIR/dev-v1.1.json \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir $OUTPUT_DIR \
+  --train_batch_size 24 \
+  --gradient_accumulation_steps 2 \
+  --optimize_on_cpu \
+  --fp16 \
+  --loss_scale 128
